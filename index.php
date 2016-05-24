4afe0bc379963424beef54db1dd795983e4cae1c
Daniel <danielgacek96>
2016-05-24 10:53:08 +0200

    Dodanie plików PHP index.php i result.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8"/>
+	<title>Wyszukanie optymalnej trasy</title>
+</head>
+
+<body>
+<form action="result.php" method="post">
+	<h1></h1>
+	Początek trasy:
+	<br></br>
+	<input type="text" name="poczatek"/>
+	
+	<br/><br/>
+	Punkt docelowy trasy:
+	<br></br>
+	<input type="text" name="koniec"/>
+	<br/><br/>
+	
+	<input type="submit" value="Szukaj!" />
+</form>
+	
+	
+</body>
+</html>
